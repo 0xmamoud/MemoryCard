3bdd94a0bde56ffed996319802b4883f0bd559f6
mahamadou <mamad2k18@gmail.com>
2023-03-31 20:19:58 +0200

    maj

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
-const cards = document.querySelectorAll('.card');
+const cardList = document.querySelectorAll('.card');
+window.addEventListener("load", getRandomCard);
 
-function getRandomCard () {
-    cards.forEach((card) => {
-            const random = Math.trunc(Math.random() * 12)
-            // console.log(random);
-            card.style.order = random;
-        })
+function getRandomCard() {
+    
+    const cardArray = Array.from(cardList);
+    cardArray.sort(() => Math.random() - 0.5);
+
+    const grid = document.querySelector('.grid');
+    cardArray.forEach(card => {
+      grid.appendChild(card);
+    });
 }
 
-window.addEventListener("load", getRandomCard);
-window.addEventListener("keypress", function () {
-    if ("space") {
-        getRandomCard()
+
+window.addEventListener("keypress", reloadCard);
+const backs = document.querySelectorAll(".back");
+
+function reloadCard() {
+
+    if("space") {
+        backs.forEach((back) => {
+           if(back.classList.contains("active")) {
+                back.classList.remove("active")
+           };
+        })
+
+        getRandomCard();
     }
-});
+};
 
-cards.forEach((card, index) => {
-    card.addEventListener("click", function() {
-        const dataAttribute = card.getAttribute("data-attr");
-        console.log(dataAttribute);
 
-    })
+cardList.forEach((card) => {
+    card.addEventListener("click", flipACard);
 })
 
+function flipACard(e) {
+    const active = e.target.children[0].children[1];
+    active.classList.add("active")
+    // console.log(active);
+    compareCard(e);
+}
 
+
+let dataAttribute = [];
+
+function compareCard(e) {
+    dataAttribute.unshift(e.target.getAttribute("data-attr"));
+    
+    if (dataAttribute.length === 1) {
+        return;
+    } else if (dataAttribute.length === 2) {
+        if (dataAttribute[0] !== dataAttribute[1]) {
+            console.log(dataAttribute);
+            setTimeout(() => {
+                const activeElements = document.querySelectorAll(".active");
+                for (let i = activeElements.length; i > 0; i--) {
+                    console.log(i);
+                    activeElements[activeElements.length - 1].classList.remove("active");
+                    activeElements[activeElements.length -2].classList.remove("active");
+                }
+            }, 1000);
+        }
+        dataAttribute = [];
+    }
+}
 
 
